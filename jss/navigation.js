cb7746dfc1bd070a0c20eb4573c6c7a6ab316f85
Lvandament5 <lvandament@gmail.com>
2021-07-14 13:09:44 +0300

    Adding HTML inputs for birthday, sex and submitting

--- jss/navigation.js
+++ jss/navigation.js
@@ -1,78 +1,41 @@
-$(function () {
-    var bindDatePicker = function () {
-        $(".date").datetimepicker({
-            format: 'YYYY-MM-DD',
-            icons: {
-                time: "fa fa-clock-o",
-                date: "fa fa-calendar",
-                up: "fa fa-arrow-up",
-                down: "fa fa-arrow-down"
-            }
-        }).find('input:first').on("blur", function () {
-            // check if the date is correct. We can accept dd-mm-yyyy and yyyy-mm-dd.
-            // update the format if it's yyyy-mm-dd
-            var date = parseDate($(this).val());
+let text = "";
 
-            if (!isValidDate(date)) {
-                //create date based on momentjs (we have that)
-                date = moment().format('YYYY-MM-DD');
-            }
+function getWeekday (year,month,day,sex){
+ 		const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-            $(this).val(date);
-        });
-    }
+    const fnames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+    const mnames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+let year2=year.toString()
+let month2 =  month-1
+let cc=year2.slice(0,2)
+let yy=year2.slice(2,4)
 
-    var isValidDate = function (value, format) {
-        format = format || false;
-        // lets parse the date to the best of our knowledge
-        if (format) {
-            value = parseDate(value);
-        }
+let weekday=Math.round(( 
+(cc/4) -2*cc-1 + 
+(5*yy/4) + 
+(26*(month2+1)/10) + 
+day 
+)%7)
+ 
+console.log(cc)
+console.log(yy)
+console.log(day)
+console.log(month2)
+console.log(sex)
+console.log(weekday)
 
-        var timestamp = Date.parse(value);
-
-        return isNaN(timestamp) == false;
-    }
-
-    var parseDate = function (value) {
-        var m = value.match(/^(\d{1,2})(\/|-)?(\d{1,2})(\/|-)?(\d{4})$/);
-        if (m)
-            value = m[5] + '-' + ("00" + m[3]).slice(-2) + '-' + ("00" + m[1]).slice(-2);
-
-        return value;
-    }
-
-    bindDatePicker();
-});
-(function bday_array() {
-    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    var fnames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
-    var mnames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
-
-    Date.prototype.getfName = function () {
-        return fnames[this.getDay()];
-    };
-    Date.prototype.getmName = function () {
-        return mnames[this.getDay()];
-    };
-    Date.prototype.getDayName = function () {
-        return days[this.getDay()];
-    };
-})();
-
-let year = parseInt(prompt("Year(XXXX)", ""));
-let month = parseInt(prompt("Month (XX)", ""));
-let day = parseInt(prompt("Day (XX)", ""));
-let sex = prompt("Are you female?(Y/N)", "");
-let month2 = month - 1;
-let bday = new Date(year, month2, day);
-let weekday = bday.getDayName();
-let fname = bday.getfName();
-let mname = bday.getmName();
-console.log("You were born on " + weekday)
+let weekdayname= days[parseInt(weekday)];
+let fname = fnames[parseInt(weekday)]
+let mname = mnames[parseInt(weekday)];
+console.log(weekdayname)
 if (sex = "Y") {
-    console.log("Your Akan name is " + fname);
+    var akanname= fname;
 } else {
-    console.log("Your Akan name is " + mname)
+    var akanname= mname
 }
+console.log(akanname)
+text += "You were born on "+weekdayname + " and your Akan name is " +akanname +"!"
+}
+getWeekday(1983,05,05,"Y")
+document.getElementById("demo").innerHTML = text;
 
